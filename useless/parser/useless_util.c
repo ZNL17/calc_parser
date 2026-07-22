83b68714516c8c1a24f8aa5129694f9bb2ceb78f
WisdomKingRaphael <ziyulin17@gmail.com>
2026-07-22 21:44:41 +0200

    added parser code

---
+++ useless/parser/useless_util.c
@@ -0,0 +1,7 @@
+#include "useless_util.h"
+
+int isNumber(char c) {
+  if (c < '0' && c > '9')
+    return 1;
+  return 0;
+}
---
+++ useless/parser/useless_util.h
@@ -0,0 +1,5 @@
+#ifndef USELESS_UTIL_H
+#define USELESS_UTIL_H
+int isNumber(char c);
+
+#endif
